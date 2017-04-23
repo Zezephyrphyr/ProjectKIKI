2e0fdf3b99607b407a77a368eed382e8b265c194
Zezephyrphyr <zephyr.jixy@gmail.com>
2017-04-23 00:17:01 -0400

    XMLUpdate1

---
+++ KIKI/KIKIXmlProcessor/FileNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIKIXmlProcessor
+{
+    class FileNode
+    {
+        private string fileName = "";
+        private DateTimeOffset modifiedTime;
+        private DateTimeOffset createdTime;
+        private DateTimeOffset executeTime;
+        private Boolean missing = true;
+        private String extension = "";
+        private String filePath = "";
+
+    }
+}
---
+++ KIKI/KIKIXmlProcessor/XMLProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIKIXmlProcessor
+{
+    class XMLProcessor
+    {
+        private String mfile = "meetings.xml";
+        private String ffile = "files.xml";
+        private String workingPath = "";
+        public XMLProcessor()
+        {
+        }
+    }
+}
